d58bc948c3a6394b8eebc3f95bbd6a645453e6af
attakei <attakei@gmail.com>
2015-06-03 17:16:46 +0900

    Use omniauth-oauth in strategy

--- lib/omniauth/strategies/backlog.rb
+++ lib/omniauth/strategies/backlog.rb
@@ -1,9 +1,9 @@
-require 'omniauth'
+require 'omniauth-oauth'
 
 module OmniAuth
   module Strategies
-    class Backlog
-      include OmniAuth::Strategy
+    class Backlog < OmniAuth::Strategies::OAuth
+      option :name, 'backlog'
     end
   end
 end

87c45d36d4f0e3a98bc13ad79dd2e73c25925057
attakei <attakei@gmail.com>
2015-06-04 20:19:12 +0900

    Add example app

---
+++ example/app.rb
@@ -0,0 +1,30 @@
+begin
+  require 'sinatra'
+  require 'omniauth'
+  require 'omniauth-backlog'
+  require 'cgi'
+rescue LoadError
+  require 'rubygems'
+  require 'sinatra'
+  require 'omniauth'
+  require 'omniauth-backlog'
+  require 'cgi'
+end
+
+use Rack::Session::Cookie
+use OmniAuth::Builder do
+  provider :backlog, 'your_backlog_space_name', 'client_id', 'client_secret'
+end
+
+
+get '/' do
+  <<-HTML
+  <a href='/auth/backlog'>Sign in with Backlog</a>
+  HTML
+end
+
+get '/auth/:name/callback' do
+  auth = request.env['omniauth.auth']
+  # do whatever you want with the information!
+  CGI::escapeHTML(auth.to_s)
+end

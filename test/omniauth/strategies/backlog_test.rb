63d43395a968089dacf733ace947799e93e54972
attakei <attakei@gmail.com>
2015-08-21 16:28:24 +0900

    test authorize/token url

--- test/omniauth/strategies/backlog_test.rb
+++ test/omniauth/strategies/backlog_test.rb
@@ -11,4 +11,22 @@ class TestOmniAuthBacklog < StrategyTestCase
     assert_equal 'https://yourspaceid.backlog.jp', strategy.client.site
   end
 
+  def test_it_has_the_correct_authorize_url
+    @options = {
+        :client_options => {
+            :site => 'https://yourspaceid.backlog.jp'
+        }
+    }
+    assert_equal 'https://yourspaceid.backlog.jp/OAuth2AccessRequest.action', strategy.client.authorize_url
+  end
+
+  def test_it_has_the_correct_token_url
+    @options = {
+        :client_options => {
+            :site => 'https://yourspaceid.backlog.jp'
+        }
+    }
+    assert_equal 'https://yourspaceid.backlog.jp/api/v2/oauth2/token', strategy.client.token_url
+  end
+
 end
